442e98c15bd07c5a75f5db38c42b2bfb2c8cc94e
JhessikKaellyLeal <jhessikkaelly.bezerraleal@gmail.com>
2026-09-21 18:03:44 -0300

    finalização do jogo

--- CampoMinado/src/main/java/Interface/Jogo.java
+++ CampoMinado/src/main/java/Interface/Jogo.java
@@ -8,7 +8,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 /**
  *
@@ -32,7 +38,12 @@ public class Jogo extends javax.swing.JFrame {
     boolean [][] abertos = new boolean[10][10];
     
     int quantidadeBombas = 15;
+    int quantidadeCasasAbertas=0;
+    
     boolean jogoEncerrado= false;
+    
+    int segundosPassados = 0;
+    Timer cronometro;
     
     
     
@@ -40,6 +51,9 @@ public class Jogo extends javax.swing.JFrame {
     // CONSTRUTOR DA CLASSE/TELA - SEM ELE A TELA NÃO FUNCIONA
     public Jogo() {
         initComponents();
+      //definir tamanho para o painel
+      painelCampo.setPreferredSize(new Dimension(900,700));
+        
         CriarTabuleiro();
     }
     
@@ -63,6 +77,16 @@ public class Jogo extends javax.swing.JFrame {
                 botao.setFocusPainted(false);
                 botao.setEnabled(false);
                 
+                final int linhaSelecionada= linha;
+                final int colunaSelecionada = coluna;
+                
+                //adicionar o evento de clique para abrir as casas
+                botao.addActionListener((ActionEvent Evento)->{
+                       abrirBotao(linhaSelecionada,colunaSelecionada); 
+                
+                            });
+                
+                
                 //adicionar o botao dentro da matriz
                 btnCampos[linha][coluna]=botao;
                 // adicionar ele dentro do painel
@@ -73,9 +97,177 @@ public class Jogo extends javax.swing.JFrame {
         
     }// fim do metodo CriarTabuleiro
     
+    public void AdicionarBombas(){
+        //Criar uma variavel Random para gerar valores aleatorios
+        Random sorteador = new Random();
+        int bombasAdicionadas = 0;
+        
+        while( bombasAdicionadas < quantidadeBombas){
+            // sortear o n° da linha e coluna que vai ficar a bomba
+            int linha = sorteador.nextInt(10);
+            int coluna = sorteador.nextInt(10);
+            //verifica se não existe bomba adicionada no local
+            if(!bombas[linha][coluna]){
+                //adicionar a bomba na matriz
+                bombas[linha][coluna]=true;
+                
+            }
+            bombasAdicionadas++;
+        }
+        
+        
+    }// fim do AdicionarBombas
+    
+    public void IniciarJogo(){
+        LimparJogo();
+        //chamar o metodo adicionarBombas
+        AdicionarBombas();
+        IniciarCronometro();
+        //depois precisamos iniciar os botoes do jogo
+        for(int colunas=0;colunas<=9;colunas++){
+            for(int linhas=0;linhas<=9;linhas++){
+                JButton botao = btnCampos[linhas][colunas];
+                //deixar os botoes visiveis e clicaveis
+                botao.setEnabled(true);
+            }//fim do 2° for
+        }//fim do 1° for
+        btnIniciar.setText("REINICIAR");
+    }//fim do iniciar jogo
     
     
-
+    public void abrirBotao(int linha, int coluna){
+        // verificar se o jogo foi finalizado
+        if(jogoEncerrado) return;
+        
+        //verificar se o botao ja foi aberto
+        if(abertos[linha][coluna]) return;
+      
+        /*se o jogo ainda estiver rodando e o botão ainda não tiver
+        sido aberto - então vamos abrir o botão*/
+        abertos[linha][coluna]=true;
+        quantidadeCasasAbertas++;
+        
+        // acessar o que tem dentro do botão
+        JButton botao = btnCampos[linha][coluna];
+        //se no botão tiver uma bomba, então vamos mostrar a bomba a ele
+        if(bombas[linha][coluna]){
+            //variavel que recebe nossa imagem
+           ImageIcon imgBomba = new ImageIcon( 
+                   getClass().getResource("/assets/bomb.png"));
+           //colocar a imagem no botao
+           botao.setIcon(imgBomba);
+           FinalizarJogo(false);
+           return;
+        }else{
+            ImageIcon imgBandeira = new ImageIcon(
+                getClass().getResource("/assets/flag.png"));
+            botao.setIcon(imgBandeira);
+            return;
+        }
+        
+    }// fim do metodo abrirBotao
+    
+    
+    // este metodo informa quando a pessoa perder ou ganhar o jogo
+    public void FinalizarJogo(boolean venceu){
+       mostrarBombas();
+        //vamos informar que o jogo acabou
+        jogoEncerrado=true;
+        cronometro.stop();
+        
+        //verificar se a pessa venceu ou não
+        if(venceu){
+            JOptionPane.showMessageDialog(
+                    this,"Parabéns você venceu!");
+            LimparJogo();
+        }else{
+            JOptionPane.showMessageDialog(
+                    this,"Ops, você perdeu o jogo!");
+            LimparJogo();
+        }
+       }//fim do FinalizarJogo
+    
+    
+    public void VerificarVitoria(){
+        // armazenar a quantidade de casas com bandeiras
+        int casasSemBomba= 100 - quantidadeBombas;
+        //se a pessoa abriu todas as bandeiras e não abriu nenhuma bomba
+        // então ela venceu o jogo, e o finalizarJogo imprime a mensagem
+        if(quantidadeCasasAbertas == casasSemBomba){
+            FinalizarJogo(true);
+        }
+  
+        
+    }
+    
+    
+    public void IniciarCronometro(){
+        // zerar o cronometro caso tenha tido um jogo anterior
+        if(cronometro !=null){
+            cronometro.stop();
+        }
+        // reseta o cronometro
+        segundosPassados = 0;
+        tfTempo.setText("00:00");
+        
+        // converter o tempo em minutos e segundos
+        // o cronometro conta de 1 em 1 segundo, e vai convertendo
+        cronometro = new Timer(1000, Evento->{
+            segundosPassados++;
+            int minutos = segundosPassados/60;
+            int horas = minutos/60;
+            int segundo = segundosPassados%60;
+            //mostrar o tempo dentro da váriavel
+            tfTempo.setText(
+            String.format("%02d:%02d:%02d",horas,minutos,segundo));
+                       
+        });
+        cronometro.start();
+        
+        
+    }
+    
+    
+    public void LimparJogo(){
+         quantidadeCasasAbertas=0;
+         jogoEncerrado=false;
+        
+         
+        for(int coluna=0;coluna<=9;coluna++){
+            for(int linha=0;linha<=9;linha++){
+                bombas[linha][coluna]=false;
+                abertos[linha][coluna]=false;
+               
+                
+                //limpeza dos botões
+                JButton botao = btnCampos[linha][coluna];
+                botao.setIcon(null);
+                
+            }//fim do 2° for
+        }//fim do 1° for
+         AdicionarBombas(); 
+         IniciarCronometro();
+        
+    }//fim do LimparJogo
+    
+    
+    public void mostrarBombas(){
+       for(int coluna=0;coluna<=9;coluna++){
+           for(int linha=0;linha<=9;linha++){
+                JButton botao = btnCampos[linha][coluna];
+                //se no botão tiver uma bomba, então vamos mostrar a bomba a ele
+                if(bombas[linha][coluna]){
+                    //variavel que recebe nossa imagem
+                   ImageIcon imgBomba = new ImageIcon( 
+                           getClass().getResource("/assets/bomb.png"));
+                   //colocar a imagem no botao
+                   botao.setIcon(imgBomba);
+                   
+                }//fim do if
+           }//fim do 2° for
+       }// fim do 1° for      
+    }// fim do mostrarBombas
+    
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -97,6 +289,7 @@ public class Jogo extends javax.swing.JFrame {
         btnIniciar.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         btnIniciar.setForeground(new java.awt.Color(255, 255, 255));
         btnIniciar.setText("INICIAR");
+        btnIniciar.addActionListener(this::btnIniciarActionPerformed);
 
         tfTempo.setEditable(false);
         tfTempo.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
@@ -147,11 +340,16 @@ public class Jogo extends javax.swing.JFrame {
                         .addComponent(tfTempo, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
                 .addComponent(painelCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addContainerGap(196, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
+        // TODO add your handling code here:
+       IniciarJogo();
+    }//GEN-LAST:event_btnIniciarActionPerformed
 
     /**
      * @param args the command line arguments
